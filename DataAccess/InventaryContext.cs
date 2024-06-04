da14c6eab7bd5da08ce662c39be7d411b192fb1e
Esteban Alvarenga <estebanalvarenga2002@gmail.com>
2024-06-03 21:02:23 -0600

    first commit

---
+++ DataAccess/InventaryContext.cs
@@ -0,0 +1,52 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class InventaryContext : DbContext
+    {
+        public DbSet<ProductEntity> Products { get; set; }
+
+        public DbSet<CategoryEntity> Categories { get; set; }
+
+        public DbSet<InputOutputEntity> InOuts { get; set; }
+
+        public DbSet<WarehouseEntity> Warehouses { get; set; }
+
+        public DbSet<StorageEntity> Storages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=ALVARENGA144-LA; Database=InventoryDb; Integrated Security=True;TrustServerCertificate=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CategoryEntity>().HasData(
+                new CategoryEntity { CategoryId = "ASH", CategoryName = "Aseo Hogar" },
+                new CategoryEntity { CategoryId = "ASP", CategoryName = "Aseo Personal" },
+                new CategoryEntity { CategoryId = "HGR", CategoryName = "Hogar" },
+                new CategoryEntity { CategoryId = "PRF", CategoryName = "Perfumería" },
+                new CategoryEntity { CategoryId = "SLD", CategoryName = "Salud" },
+                new CategoryEntity { CategoryId = "VDJ", CategoryName = "Video Juegos" }
+                );
+
+            modelBuilder.Entity<WarehouseEntity>().HasData(
+                new WarehouseEntity { WarehouseId = Guid.NewGuid().ToString(), WarehouseName = "Bodega Central", WarehouseAddress = "Calle 8 con 23" },
+                new WarehouseEntity { WarehouseId = Guid.NewGuid().ToString(), WarehouseName = "Bodega Norte", WarehouseAddress = "Calle norte con occidente" }
+                );
+
+            /*
+                modelBuilder.Entity<ProductEntity>().HasData(
+                    new ProductEntity { ProductId = "ASJ-98745", ProductName = "Crema para manos marca Tersa", ProductDescription = "", CategoryId = "PRF" },
+                    new ProductEntity { ProductId = "RPT-546587", ProductName = "Pastillas para la garganta LESUS", ProductDescription = "", CategoryId = "SLD" }
+                    );
+            */
+        }
+    }
+}
---
+++ DataAccess/Migrations/20240507063028_AddData.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[,]
+                {
+                    { "ASH", "Aseo Hogar" },
+                    { "ASP", "Aseo Personal" },
+                    { "HGR", "Hogar" },
+                    { "PRF", "Perfumería" },
+                    { "SLD", "Salud" },
+                    { "VDJ", "Video Juegos" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Warehouses",
+                columns: new[] { "WarehouseId", "WarehouseAddress", "WarehouseName" },
+                values: new object[,]
+                {
+                    { "19163712-8b85-4c2e-ae56-a2000e783d21", "Calle norte con occidente", "Bodega Norte" },
+                    { "2348624a-8038-4e9a-a7b0-e5f0f4abc951", "Calle 8 con 23", "Bodega Central" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "ASH");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "ASP");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "HGR");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "PRF");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "SLD");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "VDJ");
+
+            migrationBuilder.DeleteData(
+                table: "Warehouses",
+                keyColumn: "WarehouseId",
+                keyValue: "19163712-8b85-4c2e-ae56-a2000e783d21");
+
+            migrationBuilder.DeleteData(
+                table: "Warehouses",
+                keyColumn: "WarehouseId",
+                keyValue: "2348624a-8038-4e9a-a7b0-e5f0f4abc951");
+        }
+    }
+}
